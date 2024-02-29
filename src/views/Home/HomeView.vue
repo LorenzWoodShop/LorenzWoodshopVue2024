bb7b1eeda9db5cf594f8a9022f108dfc78efb8ab
Grayson Lorenz <graysonc.l.lorenz@gmail.com>
2024-02-29 15:40:31 -0600

    touchup

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -1,37 +1,35 @@
 <script setup lang="ts">
 import ThreeFeature from './components/threeFeature/ThreeFeature.vue';
 import ImageFeature from './components/imageFeature/ImageFeature.vue';
-
-
 </script>
 
 <template>
   <main class="container">
-    <ThreeFeature />
     <ImageFeature
       image="20211224_171105.jpg"
       title="Gaming Accessories"
       content="My bread and butter, the stuff that got me into this trade. Never lose your dice again. Have the coolest swag at the table"
-      highlight
     />
     <ImageFeature
       image="20230409_132043.jpg"
       title="Pens and Pencils"
       content="Hand turned pens, pencils, and even wands. Made from a wide variety of woods, from the humble maple to the exotic purpleheart."
+      highlight
       reverse
     />
     <ImageFeature
       image="20210704_132152.jpg"
       title="Gifts and Cool Stuff"
       content="Shop hammers, wood replica knives, jewelry boxes, and more. All made with the same gusto and care."
-      highlight
     />
     <ImageFeature
       image="20200606_101138.jpg"
       title="Furniture"
-      content="Linen chests, bookshelves, baby changing tables, and even a gaming table. I've taken on any challenge given to me."
+      content="Linen chests, bookshelves, baby changing tables, and even a gaming table. I'll make anything you want, so long as it's made from tree parts."
+      highlight
       reverse
     />
+    <ThreeFeature />
   </main>
 </template>
 
